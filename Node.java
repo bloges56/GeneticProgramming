85365484cec840f9136ae173be2cdcdfc0daf4a4
Brady <brady.drew.logan@gmail.com>
2021-09-28 13:40:10 -0400

    initial sudo code

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    //static global array of operations
+
+    //operation pointer
+    //constructor
+        //if leaf
+            //random constant or x
+        //else
+            //choose random operation
+                //generate random int x
+                //operation = operations[x]
+}
---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree
+{
+    
+
+    //point to root node
+    //constructor
+        //construct Tree to depth of random range
+        //
+
+
+}
