248c3568b731bfce48430db9e6232075d6f3a712
Brady <brady.drew.logan@gmail.com>
2021-10-19 15:00:05 -0400

    changed random node

--- FunTree.java
+++ FunTree.java
@@ -269,7 +269,7 @@ public class FunTree
         {
             return current;
         }
-        if((int)(Math.random() * 10) <= 2 * depth)
+        if((int)(Math.random() * 10) <= 2 * treeDepth)
         {
             return current;
         }
