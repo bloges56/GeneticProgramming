60465965cbe8cae0a7cc6ba73da02b66152ad83e
Brady <brady.drew.logan@gmail.com>
2021-10-20 16:45:53 -0400

    constants are floats

--- FunTree.java
+++ FunTree.java
@@ -314,9 +314,9 @@ public class FunTree
             float actual = data[i][3];
             float[] xData = {data[i][0], data[i][1], data[i][2]};
             float evaluated = evaluate(xData);
-            sum += Math.pow(actual - evaluated, 2);
+            sum += Math.abs(actual - evaluated);
         }
 
-        return sum/data.length;
+        return sum;
     }
 }
