3b7619bd14828ef890215ebb6c778966f88c1ea6
Brady <brady.drew.logan@gmail.com>
2021-10-20 08:33:31 -0400

    lowered limit for running

--- Genetic.java
+++ Genetic.java
@@ -62,7 +62,7 @@ public class Genetic {
         float fittestVal = fittestTree.getFitness();
 
         // loop while fittest > some value
-        while(fittestVal >= 0.00000168)
+        while(fittestVal >= 0.0007168)
         {
             // get the fittest in new generation
             FunTree[] nextGen = nextGen(generation);
