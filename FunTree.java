83034951e94859c72f8ce00c6838130be28e8ec1
Brady <brady.drew.logan@gmail.com>
2021-10-20 18:22:07 -0400

    decrease max depth

--- FunTree.java
+++ FunTree.java
@@ -6,7 +6,7 @@ public class FunTree
     public static Float[][] data;
 
     //set depth max depth range
-    private final int maxDepth = 10;
+    private final int maxDepth = 7;
 
     //set range of constant for leaves
     private final int constantRange = 10;
