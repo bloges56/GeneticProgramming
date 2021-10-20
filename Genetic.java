a4657ad8a4b39e2e2c596b17bc9811bf7c59e5e7
Brady <brady.drew.logan@gmail.com>
2021-10-20 08:38:43 -0400

    reduce bloat

--- Genetic.java
+++ Genetic.java
@@ -129,7 +129,7 @@ public class Genetic {
 
             int tries = 0;
             //favor less complex solutions
-            while(tries <= 30 && (selected.getDepth() > 8 || selected.getSize() > 20))
+            while(tries <= 50 && (selected.getDepth() > 8 || selected.getSize() > 20))
             {
                 selected = tournament(population);
                 tries++;
